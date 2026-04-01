1fbdda37e069e94ef3370c2f9f0b093228f4f7cb
Aditya <adityasahrawat125@gmail.com>
2026-04-01 14:52:17 +0530

    fix login

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { login } from "../controllers/auth.controller";
+import { asyncHandler } from "../middleware/asyncHandler";
 
 const router = Router();
 
-router.post("/auth/login", login);
+router.post("/auth/login", asyncHandler(login));
 
 export default router;
